92ec28c77d6b3956525adf952c75e110afea04b0
torcuata22 <marilynjmarquez@gmail.com>
2023-10-24 14:15:13 -0400

    merced profile and user models in admin

--- social/chirper/admin.py
+++ social/chirper/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import Group, User
+from .models import Profile
 # Register your models here.
+
+#unregister Groups:
+admin.site.unregister(Group)
+
+
+#Mix Profile and User (place here so that it exists before we use it in UserAdmin)
+class ProfileInline(admin.StackedInline):
+    model=Profile
+
+#Extend User Model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    #display only username field in admin:
+    fields = ['username']
+    inlines = [ProfileInline]
+
+#unregister initial User:
+admin.site.unregister(User)
+
+#register User again:
+admin.site.register(User, UserAdmin)
+
+#register Profile:
+admin.site.register(Profile)
+
+
--- social/chirper/views.py
+++ social/chirper/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'chirper/home.html', {})
+
+
+#10/24 stopped here: https://www.youtube.com/watch?v=H8MmNqDyra8&list=PLCC34OHNcOtoQCR6K4RgBWNi3-7yGgg7b&index=3
