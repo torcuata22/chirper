ad777abedd49dcd69674404cd042985f36b1c982
torcuata22 <marilynjmarquez@gmail.com>
2023-10-24 16:35:17 -0400

    profile page locked down and working

--- social/chirper/views.py
+++ social/chirper/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Profile
 
 # Create your views here.
@@ -6,7 +7,14 @@ def home(request):
     return render(request, 'chirper/home.html', {})
 
 def profile_list(request):
-    profiles = Profile.objects.exclude(user=request.user)
-    return render(request, 'chirper/profile_list.html', {"profiles":profiles})
+    if request.user.is_authenticated:
+        profiles = Profile.objects.exclude(user=request.user)
+        return render(request, 'chirper/profile_list.html', {"profiles":profiles})
 
-#10/24 stopped here: 
+    else:
+        messages.success(request, ("You must be logged in to view this page"))
+        return redirect ('home')
+
+
+
+#10/24 stopped here: https://www.youtube.com/watch?v=fk8seSR148E&list=PLCC34OHNcOtoQCR6K4RgBWNi3-7yGgg7b&index=5
